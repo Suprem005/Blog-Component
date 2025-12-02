52cc1acb34527540baf2259407bc6321fdc471e4
Suprem005 <suprem.sthao7@gmail.com>
2025-12-02 11:20:51 +0545

    done dynamic list grid view

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -37,6 +37,13 @@ const blogsData = [
       "The McLaren P1 is a limited-production, hybrid hypercar known for its extreme performance, officially produced from 2013 to 2015. It features a 3.8-liter twin-turbocharged V8 engine combined with an electric motor, producing a combined 916 horsepower, enabling it to accelerate from 0–100 km/h in 2.8 seconds. The P1 is also recognized for its advanced aerodynamics and driver-focused design, drawing heavily on McLaren's Formula 1 racing technology.",
     imageUrl: '/image/mclaren.jpg',
   },
+  {
+    id: '6',
+    title: 'Ferrari Stradale SF90',
+    description:
+      "The McLaren P1 is a limited-production, hybrid hypercar known for its extreme performance, officially produced from 2013 to 2015. It features a 3.8-liter twin-turbocharged V8 engine combined with an electric motor, producing a combined 916 horsepower, enabling it to accelerate from 0–100 km/h in 2.8 seconds. The P1 is also recognized for its advanced aerodynamics and driver-focused design, drawing heavily on McLaren's Formula 1 racing technology.",
+    imageUrl: '/image/mclaren.jpg',
+  },
 ];
 
 const BlogPage = () => {
